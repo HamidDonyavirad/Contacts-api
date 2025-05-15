e5fde25f16c4e1e4d0c3be967929f92be5ea1945
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-15 23:45:55 +0330

    feat: create views

--- project/contacts/views.py
+++ project/contacts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import ContactsUser
+from .serializers import ContactsUserSerializer
+
+
 
 # Create your views here.
+class cantactUserView(generics.ListCreateAPIView):
+    queryset = ContactsUser.objects.all()
+    serializer_class = ContactsUserSerializer

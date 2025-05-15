bbf58d6c38ddee0fb9eacc887764732da7b23563
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-15 23:44:32 +0330

    feat: create serializers

---
+++ project/contacts/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import ContactsUser
+class ContactsUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactsUser
+        fields = ['fullName','numberPhone']

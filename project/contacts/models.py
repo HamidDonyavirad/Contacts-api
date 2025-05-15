f8b3743988b080c49afb4daa1a5e50613e5cbd84
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-15 23:39:07 +0330

    feat: create models

--- project/contacts/models.py
+++ project/contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    fullName = models.CharField(max_length=100)
+    numberPhone = models.CharField(max_length=15,unique=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.fullName}-{self.numberPhone}"

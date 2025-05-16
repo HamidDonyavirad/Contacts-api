3f47c97f1edb5b982c8cb14481bf2e05755fc36d
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-16 22:17:21 +0330

    feat: configration urls app for add update and delete feature

--- project/contacts/urls.py
+++ project/contacts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('contacts/',views.ContactstUserView.as_view(),name='contact-list'),
+    path('contacts/<int:pk>',views.ContactsUserDetailView.as_view,name='contact-detail'),
 ]

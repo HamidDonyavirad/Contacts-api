b5e81ad080277bbad85fd0e4625cb33409911b69
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-16 23:05:20 +0330

    feat: Add filters and search in contacts

--- project/contacts/views.py
+++ project/contacts/views.py
@@ -8,6 +8,9 @@ from .serializers import ContactsUserSerializer
 class ContactstUserView(generics.ListCreateAPIView):
     queryset = ContactsUser.objects.all()
     serializer_class = ContactsUserSerializer
+    filterset_fields = ['fullName', 'numberPhone']
+    search_fields = ['fullName']
+    ordering_fields = ['fullName']
 
 class ContactsUserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ContactsUser.objects.all()
